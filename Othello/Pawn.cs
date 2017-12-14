b531a3e7c81a138f96ece05290f857d0c7c94414
Guillaume Noguera <guillaume.noguera@gmail.com>
2017-12-14 13:27:44 +0100

    added pawn class

---
+++ Othello/Pawn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Othello {
+    public enum Color {
+        WHITE = 0,
+        BLACK = 1
+    }
+    class Pawn {
+        private int x;
+        private int y;
+        private Color color;
+        public Pawn(int x, int y, Color c) {
+            this.x = x; 
+            this.y = y;
+            this.color = c;
+        }
+        public void reverse() {
+            this.color = this.color == Color.BLACK ? Color.WHITE : Color.BLACK;
+        }
+    }
+}
