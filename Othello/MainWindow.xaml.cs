4285b622ccd3a604dab52f150df467cf114e6d3a
Cédric Pahud <cedric.pahud@he-arc.ch>
2017-12-21 13:54:48 +0100

    debut UI

--- Othello/MainWindow.xaml.cs
+++ Othello/MainWindow.xaml.cs
@@ -20,6 +20,39 @@ namespace Othello {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            InitializeGrid();
+        }
+
+        public void InitializeGrid()
+        {
+            int nbRow = PlayGrid.RowDefinitions.Count();
+            int nbColumn = PlayGrid.ColumnDefinitions.Count();
+
+            for(int i = 0; i<nbRow; i++)
+            {
+                for(int j = 0; j<nbColumn; j++)
+                {
+                    Border bord = new Border();
+                    Button btn = new Button();
+                    Grid.SetColumn(btn, j);
+                    Grid.SetRow(btn, i);
+                    Grid.SetColumn(bord, j);
+                    Grid.SetRow(bord, i);
+                    bord.BorderThickness = new Thickness(2.0);
+                    bord.BorderBrush = Brushes.Black;
+                    btn.Click += grid_Item_Click;
+                    PlayGrid.Children.Add(bord);
+                    PlayGrid.Children.Add(btn);
+                }
+            }
+        }
+
+        private void grid_Item_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            int x = (int)btn.GetValue(Grid.RowProperty);
+            int y = (int)btn.GetValue(Grid.ColumnProperty);
+            MessageBox.Show("row" + x.ToString() + "column" + y.ToString());
         }
     }
 }
