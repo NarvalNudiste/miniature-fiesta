ed90560fc40c7f2bda894fe5d3d75b284bb7b0a4
Cédric Pahud <cedric.pahud@he-arc.ch>
2018-01-17 22:00:47 +0100

    Fonction de la mort qui tue

--- Othello/Game.cs
+++ Othello/Game.cs
@@ -96,7 +96,7 @@ namespace Othello
             }
             return s;
         }
-        private bool CheckLine(int x, int y, int direction, int color, bool stockCurrentLocation, ArrayList ary = null, int[,] game = null, ArrayList ops) {
+        private bool CheckLine(int x, int y, int direction, int color, bool stockCurrentLocation, ArrayList ary = null, int[,] game = null, ArrayList ops = null) {
             if (game == null) {
                 game = this.board;
             }
@@ -164,13 +164,13 @@ namespace Othello
             return false;
         }
 
-        private ArrayList Ops(int[,] game) {
+        private ArrayList Ops(int[,] game,int player) {
             ArrayList ops = new ArrayList();
             for (int y = 0; y < this.boardSize; y++) {
                 for (int x = 0; x< this.boardSize; x++) {
                     for (int i = 0; i < 10; i++) {
                         if (i != 5) {
-                            CheckLine(x, y, i, this.currentPlayer, false, null, game, ops);
+                            CheckLine(x, y, i, player, false, null, game, ops);
                         }
                     }
                 }
@@ -262,24 +262,24 @@ namespace Othello
         }
 
         public Tuple<int, int> GetNextMove(int[,] game, int level, bool whiteTurn) {
-            Tuple<int, int, int> move = alphabeta(board, 5, 1, Score(board),whiteTurn?0:1);
+            Tuple<Double, int, int> move = alphabeta(board, 5, 1, 0,whiteTurn?0:1);
             return new Tuple<int, int>(move.Item2, move.Item3);
         }
 
-        public Tuple<int,int,int> alphabeta(int[,] root,int depth, int minOrMax, int parentValue, int player)
+        public Tuple<Double,int,int> alphabeta(int[,] root,int depth, int minOrMax, Double parentValue, int player)
         {
             if (depth == 0 || Final(player,root))
             {
                 //retourne -1 pour la position a jouer si on est au fond
-                return new Tuple<int,int,int>(Score(root), -1, -1);
+                return new Tuple<Double,int,int>(Score(root,player), -1, -1);
             }
             //je crois pour test
-            int optVal = int.MinValue;
+            Double optVal = Double.MinValue;
             int[] optOp = null;
-            foreach (int[] op in Ops(root))
+            foreach (int[] op in Ops(root,player))
             {
                 int[,] newNode = Apply(op[0],op[1],player==0,root);
-                int val = alphabeta(newNode, depth - 1, -minOrMax, optVal,player==0?1:0).Item1;
+                Double val = alphabeta(newNode, depth - 1, -minOrMax, optVal,player==0?1:0).Item1;
                 if (val*minOrMax>parentValue*minOrMax)
                 {
                     optVal = val;
@@ -290,12 +290,125 @@ namespace Othello
                     }
                 }
             }
-            return new Tuple<int, int, int>(optVal,optOp[0],optOp[1]);
+            return new Tuple<Double, int, int>(optVal,optOp[0],optOp[1]);
         }
 
-        public int Score(int[,] board)
+        //https://github.com/kartikkukreja/blog-codes/blob/master/src/Heuristic%20Function%20for%20Reversi%20(Othello).cpp
+        public Double Score(int[,] board, int player)
         {
-            return 0;
+            int my_color = player;
+            int opp_color = player == 0 ? 1 : 0;
+            int my_tiles = 0, opp_tiles = 0, i, j, k, my_front_tiles = 0, opp_front_tiles = 0, x, y;
+            double p = 0, c = 0, l = 0, m = 0, f = 0, d = 0;
+
+            int[] X1 = { -1, -1, 0, 1, 1, 1, 0, -1 };
+            int[] Y1 = { 0, 1, 1, 1, 0, -1, -1, -1 };
+
+            int[,]V = {{20, -3, 11, 8, 8, 11, -3, 20},
+                       {-3, -7, -4, 1, 1, -4, -7, -3},
+                       { 11, -4, 2, 2, 2, 2, -4, 11},
+                       { 8, 1, 2, -3, -3, 2, 1, 8},
+                       { 8, 1, 2, -3, -3, 2, 1, 8},
+                       { 11, -4, 2, 2, 2, 2, -4, 11},
+                       { -3, -7, -4, 1, 1, -4, -7, -3},
+                       { 20, -3, 11, 8, 8, 11, -3, 20} };
+
+            // Piece difference, frontier disks and disk squares
+	        for(i=0; i<8; i++)
+		        for(j=0; j<8; j++)  {
+			        if(board[i,j] == my_color)  {
+				        d += V[i,j];
+				        my_tiles++;
+			        } else if(board[i,j] == opp_color)  {
+				        d -= V[i,j];
+				        opp_tiles++;
+			        }
+			        if(board[i,j] != -1)   {
+				        for(k=0; k<8; k++)  {
+					        x = i + X1[k]; y = j + Y1[k];
+					        if(x >= 0 && x< 8 && y >= 0 && y< 8 && board[x,y] == -1) {
+						        if(board[i,j] == my_color)  my_front_tiles++;
+						        else opp_front_tiles++;
+						        break;
+					        }
+				        }
+			        }
+		        }
+	        if(my_tiles > opp_tiles)
+		        p = (100.0 * my_tiles)/(my_tiles + opp_tiles);
+	        else if(my_tiles<opp_tiles)
+
+                p = -(100.0 * opp_tiles)/(my_tiles + opp_tiles);
+	        else p = 0;
+
+	        if(my_front_tiles > opp_front_tiles)
+		        f = -(100.0 * my_front_tiles)/(my_front_tiles + opp_front_tiles);
+	        else if(my_front_tiles<opp_front_tiles)
+
+                f = (100.0 * opp_front_tiles)/(my_front_tiles + opp_front_tiles);
+	        else f = 0;
+
+            // Corner occupancy
+	        my_tiles = opp_tiles = 0;
+	        if(board[0,0] == my_color) my_tiles++;
+	        else if(board[0,0] == opp_color) opp_tiles++;
+	        if(board[0,7] == my_color) my_tiles++;
+	        else if(board[0,7] == opp_color) opp_tiles++;
+	        if(board[7,0] == my_color) my_tiles++;
+	        else if(board[7,0] == opp_color) opp_tiles++;
+	        if(board[7,7] == my_color) my_tiles++;
+	        else if(board[7,7] == opp_color) opp_tiles++;
+	        c = 25 * (my_tiles - opp_tiles);
+
+            // Corner closeness
+	        my_tiles = opp_tiles = 0;
+	        if(board[0,0] == -1)   {
+		        if(board[0,1] == my_color) my_tiles++;
+		        else if(board[0,1] == opp_color) opp_tiles++;
+		        if(board[1,1] == my_color) my_tiles++;
+		        else if(board[1,1] == opp_color) opp_tiles++;
+		        if(board[1,0] == my_color) my_tiles++;
+		        else if(board[1,0] == opp_color) opp_tiles++;
+	        }
+	        if(board[0,7] == -1)   {
+		        if(board[0,6] == my_color) my_tiles++;
+		        else if(board[0,6] == opp_color) opp_tiles++;
+		        if(board[1,6] == my_color) my_tiles++;
+		        else if(board[1,6] == opp_color) opp_tiles++;
+		        if(board[1,7] == my_color) my_tiles++;
+		        else if(board[1,7] == opp_color) opp_tiles++;
+	        }
+	        if(board[7,0] == -1)   {
+		        if(board[7,1] == my_color) my_tiles++;
+		        else if(board[7,1] == opp_color) opp_tiles++;
+		        if(board[6,1] == my_color) my_tiles++;
+		        else if(board[6,1] == opp_color) opp_tiles++;
+		        if(board[6,0] == my_color) my_tiles++;
+		        else if(board[6,0] == opp_color) opp_tiles++;
+	        }
+	        if(board[7,7] == -1)   {
+		        if(board[6,7] == my_color) my_tiles++;
+		        else if(board[6,7] == opp_color) opp_tiles++;
+		        if(board[6,6] == my_color) my_tiles++;
+		        else if(board[6,6] == opp_color) opp_tiles++;
+		        if(board[7,6] == my_color) my_tiles++;
+		        else if(board[7,6] == opp_color) opp_tiles++;
+	        }
+	        l = -12.5 * (my_tiles - opp_tiles);
+
+            // Mobility
+	        my_tiles = Ops(board,my_color).Count;
+            opp_tiles = Ops(board, opp_color).Count;
+	        if(my_tiles > opp_tiles)
+		        m = (100.0 * my_tiles)/(my_tiles + opp_tiles);
+	        else if(my_tiles<opp_tiles)
+
+                m = -(100.0 * opp_tiles)/(my_tiles + opp_tiles);
+	        else m = 0;
+
+            // final weighted score
+	        double score = (10 * p) + (801.724 * c) + (382.026 * l) + (78.922 * m) + (74.396 * f) + (10 * d);
+            return score;
         }
 
         public int[,] GetBoard() {
