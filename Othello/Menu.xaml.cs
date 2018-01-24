858b733f594c2295f0cbd602bf8d1a0cb13c1a0f
Cédric Pahud <cedric.pahud@he-arc.ch>
2018-01-24 13:25:46 +0100

    changement UI et ajout d'un menu

---
+++ Othello/Menu.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Othello
+{
+    /// <summary>
+    /// Logique d'interaction pour Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void new_game(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Hide();
+            mainWindow.Closing += new CancelEventHandler(show_menu);
+        }
+
+        private void load_game(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void exit_game(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void show_menu(object sender, CancelEventArgs e)
+        {
+            this.Show();
+        }
+    }
+}
